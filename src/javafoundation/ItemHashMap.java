6ab33a46544021b7657e56c065bec7bb8024e812
0xack13 <0xack13@gmail.com>
2014-11-28 14:31:10 +0300

    Initial commit

---
+++ src/javafoundation/ItemHashMap.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafoundation;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author 0xack13
+ */
+public class ItemHashMap {
+    //Private Hashmap
+    private HashMap<Integer, Item> hm;
+    //current number of created "ItemHashMap"
+    public static int hmCounter = 0;
+    
+    public void ItemHashMap() {
+        hm = new HashMap<Integer, Item>();
+    }
+    
+    
+}
