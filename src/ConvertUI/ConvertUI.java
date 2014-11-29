fbadd4c247c73b682b52781f958d3eb67675e465
0xack13 <0xack13@gmail.com>
2014-11-29 15:49:43 +0300

    Add buttons

--- src/ConvertUI/ConvertUI.java
+++ src/ConvertUI/ConvertUI.java
@@ -23,31 +23,25 @@ import javax.swing.UIManager;
 // This example demonstrates the use of JButton, JTextField and JLabel.
 public class ConvertUI implements ActionListener {
   JFrame converterFrame;
-
   JPanel converterPanel1, converterPanel2, converterPanel3;
-
   JTextField tempCelsius;
-
   JLabel celsiusLabel, fahrenheitLabel;
-
   JButton convertTempC2F, convertTempF2C, convertTempClear;
 
   // Constructor
   public ConvertUI() {
     // Create the frame and container.
     converterFrame = new JFrame("Convert Celsius to Fahrenheit");
+    
+    converterFrame.setSize(400, 200);
+    
     converterPanel1 = new JPanel();
     converterPanel1.setLayout(new BorderLayout());
-    
     converterPanel2 = new JPanel();
     converterPanel2.setLayout(new BorderLayout());
-    
     converterPanel3 = new JPanel();
     converterPanel3.setLayout(new BorderLayout());
-
-    // Add the widgets.
-    addWidgets();
-
+    
     // Add the panel to the frame.
     converterFrame.getContentPane()
         .add(converterPanel1, BorderLayout.NORTH);
@@ -55,6 +49,11 @@ public class ConvertUI implements ActionListener {
         .add(converterPanel2, BorderLayout.CENTER);
     converterFrame.getContentPane()
         .add(converterPanel3, BorderLayout.SOUTH);
+    
+    // Add the widgets.
+    addWidgets();
+
+    
 
     // Exit when the window is closed.
     converterFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -72,8 +71,12 @@ public class ConvertUI implements ActionListener {
         "Convert temperature");
     tempCelsius = new JTextField(2);
     celsiusLabel = new JLabel("Celsius", SwingConstants.LEFT);
-    convertTempC2F = new JButton(icon);
+    
     fahrenheitLabel = new JLabel("Fahrenheit", SwingConstants.LEFT);
+    
+    convertTempC2F = new JButton("To Fahrenheit");
+    convertTempClear = new JButton("Clear");
+    convertTempF2C = new JButton("To Celcius");
 
     celsiusLabel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     fahrenheitLabel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
@@ -82,10 +85,14 @@ public class ConvertUI implements ActionListener {
     convertTempC2F.addActionListener(this);
 
     // Add widgets to container.
-    converterPanel.add(tempCelsius);
-    converterPanel.add(celsiusLabel);
-    converterPanel.add(convertTempC2F);
-    converterPanel.add(fahrenheitLabel);
+    converterPanel2.add(tempCelsius, BorderLayout.CENTER);
+    converterPanel2.add(celsiusLabel, BorderLayout.NORTH);
+    //converterPanel2.add(convertTempC2F, BorderLayout.CENTER);
+    converterPanel2.add(fahrenheitLabel, BorderLayout.CENTER);
+    
+    converterPanel3.add(convertTempF2C, BorderLayout.NORTH);
+    converterPanel3.add(convertTempC2F, BorderLayout.CENTER);
+    converterPanel3.add(convertTempClear, BorderLayout.SOUTH);
   }
 
   // Implementation of ActionListener interface.
